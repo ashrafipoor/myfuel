fc4b129a29e3b9ebac5227a743828ad6c3ead95b
Hossein Ashrafipoor <ashrafipoor@gmail.com>
2025-09-05 10:24:08 +0330

    clean api docs

--- src/organizations/organizations.module.ts
+++ src/organizations/organizations.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { OrganizationsService } from './organizations.service';
-import { OrganizationsController } from './organizations.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Organization } from './entities/organization.entity';
 import { OrgBalance } from './entities/org-balance.entity';
@@ -8,7 +7,7 @@ import { BalanceLedger } from './entities/balance-ledger.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Organization, OrgBalance, BalanceLedger])],
-  controllers: [OrganizationsController],
+  controllers: [],
   providers: [OrganizationsService],
   exports: [TypeOrmModule],
 })
